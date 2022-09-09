bd1e655bdf26aa532167c25f5a42656d0d0d541c
Lajos Veres <lajos.veres@r3.com>
2022-09-09 11:20:10 +0100

    CORE-5114 Open customize for now to let us use that in our test (probably not idea on longer term.)

--- libs/serialization/serialization-kryo/src/main/kotlin/net/corda/kryoserialization/DefaultKryoCustomizer.kt
+++ libs/serialization/serialization-kryo/src/main/kotlin/net/corda/kryoserialization/DefaultKryoCustomizer.kt
@@ -43,7 +43,7 @@ class DefaultKryoCustomizer {
     companion object {
         private const val LOGGER_ID = Int.MAX_VALUE
 
-        internal fun customize(
+        fun customize(
             kryo: Kryo,
             serializers: Map<Class<*>, Serializer<*>>,
             classResolver: CordaClassResolver,
